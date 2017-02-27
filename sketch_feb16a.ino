183978507905e56a7fffb02e44dacf4c4b1553c3
Red the Dragon <crayman122@gmail.com>
2017-02-27 10:03:47 -0500

    uploading to this repo:   sketch_feb16a.ino

--- sketch_feb16a.ino
+++ sketch_feb16a.ino
@@ -10,19 +10,19 @@ void setup(){
   Serial.println("http://redthedragon.tk/portfolio.html");
   Serial.println("");
   //constants
-  const int echoPin1 = 9;
-  const int triggerPin1 = 10;
-  const int echoPin2 = 11;
-  const int triggerPin2 = 12;
-  const int echoPin3 = 13;
-  const int triggerPin3 = 14;
-  const int motorLF = 15;
-  const int motorLB = 16;
-  const int motorRF = 17;
-  const int motorRB = 18;
-  const int ledR = 19;
-  const int ledG = 20;
-  const int ledB = 21;
+  int echoPin1 = 9;
+  int triggerPin1 = 10;
+  int echoPin2 = 11;
+  int triggerPin2 = 12;
+  int echoPin3 = 13;
+  int triggerPin3 = 14;
+  int motorLF = 15;
+  int motorLB = 16;
+  int motorRF = 17;
+  int motorRB = 18;
+  int ledR = 19;
+  int ledG = 20;
+  int ledB = 21;
   //pinModes
   pinMode(triggerPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
@@ -37,13 +37,32 @@ void setup(){
   pinMode(ledR, OUTPUT);
   pinMode(ledG, OUTPUT);
   pinMode(ledB, OUTPUT);
-  //led test
+  //power on self test
+  Serial.println("POS Test");
+  Serial.println("");
+  Serial.println("LED Test");
+  Serial.println("");
   digitalWrite(ledR, HIGH);
   digitalWrite(ledR, LOW);
   digitalWrite(ledG, HIGH);
   digitalWrite(ledG, LOW);
   digitalWrite(ledB, HIGH);
   digitalWrite(ledB, LOW);
+  Serial.println("Ultrasonic test");
+  Serial.println("");
+  Serial.println("Sensor 1");
+  Serial.println(""));
+  Serial.println(getDistance(triggerPin1, echoPin1);
+  Serial.println("");
+  Serial.println("Sensor 2");
+  Serial.println("");
+  Serial.println(getDistance(triggerPin2, echoPin2);
+  Serial.println("");
+  Serial.println("Sensor 3");
+  Serial.println("");
+  Serial.println(getDistance(triggerPin3, echoPin3);
+  Serial.println("");
+  Serial.println("POS Test Complete");
 }
 void loop(){
   //do loop stuff
@@ -61,22 +80,22 @@ void loop(){
     delay(500);
     digitalWrite(motorRB, LOW);
     digitalWrite(motorLF, LOW);
+  }
     //else if there isnt anything right, then go right.
-    else if(getDistance(triggerPin3, echoPin3) > 10){
-      digitalWrite(motorRF, HIGH);
-      digitalWrite(motorLB, HIGH);
-      delay(500);
-      digitalWrite(motorRF, HIGH);
-      digitalWrite(motorLB, HIGH);
-    }
-    else{
-      digitalWrite(motorRB, HIGH);
-      digitalWrite(motorLF, HIGH);
-      delay(1000);
-      digitalWrite(motorRB, LOW);
-      digitalWrite(motorLF, LOW);
-    }
-    
+  else if(getDistance(triggerPin3, echoPin3) > 10){
+    digitalWrite(motorRF, HIGH);
+    digitalWrite(motorLB, HIGH);
+    delay(500);
+    digitalWrite(motorRF, HIGH);
+    digitalWrite(motorLB, HIGH);
+  }
+  else{
+    digitalWrite(motorRB, HIGH);
+    digitalWrite(motorLF, HIGH);
+    delay(1000);
+    digitalWrite(motorRB, LOW);
+    digitalWrite(motorLF, LOW);
+  }  
 }
 int getDistance(int trigPin, int echoPin){
   digitalWrite(trigPin, LOW);
